2babd7ae22addf6b7e6f1210a3d1e8d13b3fad94
amy-u-zhang <amy.u.zhang@gmail.com>
2016-04-01 14:32:41 -0500

    Delete aws_user_login.sh

--- sidserver/script/aws_user_login.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-AWS_ACCESS_KEY_ID=$1
-AWS_ACCESS_SECRET_KEY=$2
-AWS_ACCOUNT=CPS
-
-curl -i 'http://10.245.121.24:5000/v2.0/tokens/aws/user_login' -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d '{"auth": {"AWS_ACCESS_KEY_ID":\"$AWS_ACCESS_KEY_ID\", "AWS_ACCESS_SECRET_KEY":\"$AWS_ACCESS_SECRET_KEY\", "AWS_ACCOUNT":"CPS"}}'
